2bc4831e32da970132468f06195e90e845772820
MarcioRMed <contato.microinfo@gmail.com>
2022-06-17 03:54:57 -0300

    primeira parte

--- App.js
+++ App.js
@@ -1,20 +1,136 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { 
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput,
+  ViewBase,
+} from "react-native";
 
-export default function App() {
+export default function calcimc() {
+
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [resultadoImc, setResultadoImc] = useState(0);
+
+  console.log('peso ' , peso)
+  console.log('altura ', altura)
+  console.log('resultado ', resultadoImc)
+  
+  
+  // const calcularImc(){}l
+
+  const calcularImc=()=>{
+  
+  if(peso==0 || !peso){
+      alert('Informe o Peso')
+      return
+    }
+    
+    if(altura==0 || !altura){
+      alert('Informe a altura')
+      return
+    }
+    
+    
+    // fórmula
+    // imc = peso/(altura * altura)
+    
+    // const imc = peso/(Math.pow(altura,2))
+    // setResultadoImc(imc.toFixed(1))
+  
+    const imc = peso/(altura * altura)
+    setResultadoImc(imc)
+
+    console.log('imc', imc)
+    console.log('peso ' , peso)
+    console.log('altura ', altura)
+    console.log('resultado ', resultadoImc)
+  
+  }
+
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      
+      <View style={styles.view}>
+        <Text>Calculadora de Indice de Massa Corporal IMC </Text>
+      </View>
+
+      <View style={styles.view}>
+        <Text>Informe seu Peso</Text>
+        <TextInput
+          style={styles.textInput}
+          autoFocus={true}
+          keyboardType={"numeric"}
+          onChange={(text) => setPeso(text)}
+        ></TextInput>
+      </View>
+
+      <View style={styles.view}>
+        <Text>Informe seu Peso</Text>
+        <TextInput
+          style={styles.textInput}
+          autoFocus={false}
+          keyboardType={"numeric"}
+          onChange={(text) => setAltura(text)}
+        ></TextInput>
+      </View>
+
+{/* --- Botão ---  */}
+      <View style={styles.view}>
+        <TouchableHighlight
+        style={styles.btnCalcular}
+        onPress={()=> calcularImc()}
+        >
+          <Text style={styles.btnTexto}>Calcular IMC</Text>
+        </TouchableHighlight>
+      </View>
+
+
+    <View style={styles.view} >
+      <Text>Resultado: {resultadoImc}</Text>
+    </View>
+
+
     </SafeAreaView>
   );
 }
 
+
+// --- Estilos ---
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+  },
+
+  textInput: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 10,
+    borderRadius: 10,
+  },
+
+  view: {
+    marginBottom: 20,
+  },
+
+  btnCalcular:{
+    backgroundColor:'#048',
+    justifyContent:'center',
+    alignItems:'center',
+    padding:10,
+    borderRadius:20,
+
+  },
+
+  btnTexto:{
+    fontSize:15,
+    textTransform:'uppercase',
+    color:'#fff',
+
   },
 });
